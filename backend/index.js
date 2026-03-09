73787362a45c4fb7b373df3a6bb5c7649f718dec
ramanbhatia3 <bhattiramandeep3105@gmail.com>
2026-03-09 23:33:02 +0530

    add admin skeleton routes and router

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 const express = require("express")
 
 const { userRouter } = require("./routes/user")
+const { adminRouter } = require("./routes/admin")
 const { courseRouter } = require("./routes/course")
 
 const app = express()
 
 app.use(express.json())
 
-app.use("/user", userRouter)
-app.use("/course", courseRouter)
+app.use("/api/user", userRouter)
+app.use("/api/admin", adminRouter)
+app.use("/api/course", courseRouter)
 
 app.listen(3000)
---
+++ backend/routes/admin.js
@@ -0,0 +1,39 @@
+const { Router } = require("express")
+
+const adminRouter = Router()
+
+adminRouter.post("/signup", function(req, res){
+    res.json({
+        message: "admin signup endpoint"
+    })
+})
+
+adminRouter.post("/login", function(req, res){
+    res.json({
+        message: "admin login endpoint"
+    })
+})
+
+adminRouter.use(adminMiddleware)
+
+adminRouter.post("/course", function(req, res){
+    res.json({
+        message: "admin course creation endpoint"
+    })
+})
+
+adminRouter.put("/course", function(req, res){
+    res.json({
+        message: "admin course edit endpoint"
+    })
+})
+
+adminRouter.get("/course/preview", function(req, res){
+    res.json({
+        message: "admin course get endpoint"
+    })
+})
+
+module.exports = {
+    adminRouter: adminRouter
+}
